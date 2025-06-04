99f1a184ce6fba0584b4b995a3882818b9851060
vaibhav <vaibhavlahare3618@gmail.com>
2025-06-04 18:43:02 +0530

    3-tier-docker-project

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS testdb;
-USE testdb;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-INSERT INTO users (name) VALUES ('Alice'), ('Bob'), ('Charlie');
